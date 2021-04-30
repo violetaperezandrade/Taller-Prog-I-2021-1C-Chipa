4a260bdad644207a1a0f51c08082df4265cb5431
GonzaloDiazMulligan <gonzalodiazmulligan@gmail.com>
2021-04-30 09:42:17 -0300

    Entity manager

---
+++ src/model/EntityManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gonza on 30/4/21.
+//
+
+#include "EntityManager.h"
---
+++ src/model/EntityManager.h
@@ -0,0 +1,15 @@
+#ifndef ENTITYMANAGER_H
+#define ENTITYMANAGER_H
+
+#include "vector"
+
+class EntityManager {
+private:
+    std::vector<> vector;
+
+public:
+    EntityManager();
+    ~EntityManager();
+};
+
+#endif //ENTITYMANAGER_H
