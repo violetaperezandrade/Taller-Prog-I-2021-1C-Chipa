b2960f7b3964e04b6e667bfe1cd14f7f09a435d8
GonzaloDiazMulligan <gonzalodiazmulligan@gmail.com>
2021-05-02 11:40:21 -0300

    CollisionManager class start

---
+++ src/model/CollisionManager.cpp
@@ -0,0 +1,8 @@
+#include "CollisionManager.h"
+
+CollisionManager::CollisionManager(int &character,
+                                   std::vector<Entity> &vector) {}
+
+CollisionManager::~CollisionManager() {}
+
+void CollisionManager::move(Entity &entity) {}
