427781cc550f46b66133ef170322187b475a3f72
MauRodriguez <mrodriguezb@fi.uba.ar>
2021-05-04 10:50:27 -0300

    Added getters and setters in Entity class

--- src/model/Entity.h
+++ src/model/Entity.h
@@ -1,22 +1,37 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
-#include "Coordinate.h"
-#include "Size.h"
-
 class Entity {
 protected:
-    int x;
-    int y;
+    int posX;
+    int posY;
     int width;
     int height;
     int speedX;
     int speedY;
 
 public:
-    Entity();
     Entity(int x, int y, int width, int height);
-    ~Entity();
+
+    Entity(int posX, int posY, int width, int height, int speedX, int speedY);
+
+    void setPosY(int& i);
+
+    void setPosX(int& i);
+
+    void setSpeedX(int& i);
+
+    void setSpeedY(int& i);
+
+    int getPosY();
+
+    int getPosX();
+
+    int getSpeedX();
+
+    int getSpeedY();
+
+    virtual ~Entity();
 };
 
 #endif //ENTITY_H
