22ddc55ac1b0e422af928d6218d631ea135742bd
MauRodriguez <mrodriguezb@fi.uba.ar>
2021-05-18 14:55:36 -0300

    Refresh after render

--- src/view/View.cpp
+++ src/view/View.cpp
@@ -116,6 +116,9 @@ void View::free(SDL_Texture* texture){
     }
 }
 void View::refresh(){
+    SDL_SetRenderDrawColor(windowRenderer,0,0,0,0xFF);
+    SDL_RenderClear(windowRenderer);
+    SDL_RenderPresent(windowRenderer);
     Message entityInfo = game.getStatus();
     while(!entityInfo.isEmpty()){
         char entityType;
@@ -127,9 +130,6 @@ void View::refresh(){
         entityInfo.getEntityInfo(entityType,posX,posY,width,height,state);
         render(posX,posY,width,height,state,entityType);
     }
-    SDL_SetRenderDrawColor(windowRenderer,0,0,0,0xFF);
-    SDL_RenderClear(windowRenderer);
-    SDL_RenderPresent(windowRenderer);
 }
 void View::renderFilledQuad(){
     SDL_Rect fillRect = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
