1bce8b1458438c9534a6c7a6f65ed43fcbc6f27b
GonzaloDiazMulligan <gonzalodiazmulligan@gmail.com>
2021-05-18 23:32:18 -0300

    fix platform height for collision

--- src/model/CollisionManager.cpp
+++ src/model/CollisionManager.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 
 #define PI 3.14159265
+#define PLATFORM_HEIGHT 20;
 
 CollisionManager::CollisionManager(Character &character, std::vector<Entity> &vector, Logger& logger) :
     character(character),
@@ -92,8 +93,8 @@ void CollisionManager::moveCharacter() {
         x = map_width;
     }
 
-    if (y <= height){
-        y = height;
+    if (y <= height+PLATFORM_HEIGHT){
+        y = height+PLATFORM_HEIGHT;
         character.land();
         logger.debugMsg("Character has landed");
     } else if (y > map_width){
