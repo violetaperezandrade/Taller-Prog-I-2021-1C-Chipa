0f353915c547c56ebb50c730020dd17a4cc50b2b
MauRodriguez <mrodriguezb@fi.uba.ar>
2021-06-01 19:25:03 -0300

    Add Peer class

---
+++ src/server/Peer.h
@@ -0,0 +1,16 @@
+#ifndef PEER_H
+#define PEER_H
+
+#include "../common/Socket.h"
+
+class Peer {
+private:
+    Socket peer;
+    //Thread Sender
+    //Thread Receiver
+public:
+
+};
+
+
+#endif //PEER_H
