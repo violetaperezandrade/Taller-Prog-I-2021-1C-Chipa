48677b5b726e58560eb16bf53db76f19bada55e3
violetaperezandrade <viperez@fi.uba.ar>
2021-06-05 22:01:10 -0300

    use WaitEvent

--- src/client/Login.cpp
+++ src/client/Login.cpp
@@ -115,7 +115,8 @@ int Login::runLoginWindow() {
     while(!quit){
         bool renderText = false;
 
-        while(SDL_PollEvent(&e) != 0){
+        //while(SDL_PollEvent(&e) != 0){
+        while(SDL_WaitEvent(&e) != 0){
             if(e.type == SDL_QUIT) quit = true;
             if(e.type == SDL_KEYDOWN){
 
