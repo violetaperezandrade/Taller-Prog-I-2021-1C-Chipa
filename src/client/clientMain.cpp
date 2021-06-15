221aa4251758e33a4ec08879654ed026703253c8
MauRodriguez <mrodriguezb@fi.uba.ar>
2021-06-15 20:17:34 -0300

    Fix wrong port

--- src/client/clientMain.cpp
+++ src/client/clientMain.cpp
@@ -4,10 +4,11 @@
 
 int main(int argc, char** argv){
     char host[10] = "localhost";
+    char port[5] = "8080";
     Logger logger(argv[2]);
     Config config(argv[0], logger);
     logger.setLevel(config.getDebug());
-    Client client(host, argv[0], logger, config);
+    Client client(host, port, logger, config);
     client.run();
     return 0;
 }
