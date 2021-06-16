1b8e23829df515b0070742d1b483828465c91cbb
GonzaloDiazMulligan <gonzalodiazmulligan@gmail.com>
2021-06-15 23:47:02 -0300

    input protocol uses logger

--- src/common/protocols/InputProtocol.cpp
+++ src/common/protocols/InputProtocol.cpp
@@ -1,42 +1,42 @@
 #include "InputProtocol.h"
 
-void InputProtocol::sendPressUpEvent(Socket& socket){
+void InputProtocol::sendPressUpEvent(Socket& socket, Logger& logger){
     char input = 1;
-    socket.send(&input, 1);
+    socket.send(&input, 1, logger);
 }
-void InputProtocol::sendReleaseUpEvent(Socket& socket){
+void InputProtocol::sendReleaseUpEvent(Socket& socket, Logger& logger){
     char input = 2;
-    socket.send(&input, 1);
+    socket.send(&input, 1, logger);
 }
-void InputProtocol::sendPressDownEvent(Socket& socket){
+void InputProtocol::sendPressDownEvent(Socket& socket, Logger& logger){
     char input = 3;
-    socket.send(&input, 1);
+    socket.send(&input, 1, logger);
 }
-void InputProtocol::sendReleaseDownEvent(Socket& socket){
+void InputProtocol::sendReleaseDownEvent(Socket& socket, Logger& logger){
     char input = 4;
-    socket.send(&input, 1);
+    socket.send(&input, 1, logger);
 }
-void InputProtocol::sendPressLeftEvent(Socket& socket){
+void InputProtocol::sendPressLeftEvent(Socket& socket, Logger& logger){
     char input = 5;
-    socket.send(&input, 1);
+    socket.send(&input, 1, logger);
 }
-void InputProtocol::sendReleaseLeftEvent(Socket& socket){
+void InputProtocol::sendReleaseLeftEvent(Socket& socket, Logger& logger){
     char input = 6;
-    socket.send(&input, 1);
+    socket.send(&input, 1, logger);
 }
-void InputProtocol::sendPressRightEvent(Socket& socket){
+void InputProtocol::sendPressRightEvent(Socket& socket, Logger& logger){
     char input = 7;
-    socket.send(&input, 1);
+    socket.send(&input, 1, logger);
 }
-void InputProtocol::sendReleaseRightEvent(Socket& socket){
+void InputProtocol::sendReleaseRightEvent(Socket& socket, Logger& logger){
     char input = 8;
-    socket.send(&input, 1);
+    socket.send(&input, 1, logger);
 }
-void InputProtocol::sendPressJumpEvent(Socket& socket){
+void InputProtocol::sendPressJumpEvent(Socket& socket, Logger& logger){
     char input = 9;
-    socket.send(&input, 1);
+    socket.send(&input, 1, logger);
 }
-void InputProtocol::sendReleaseJumpEvent(Socket& socket){
+void InputProtocol::sendReleaseJumpEvent(Socket& socket, Logger& logger){
     char input = 10;
-    socket.send(&input, 1);
+    socket.send(&input, 1, logger);
 }
