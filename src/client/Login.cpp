052dfa4d1ef7c76a4b0e3899e995e2288be1a2b4
lautis1 <lautaro.stroia@gmail.com>
2021-06-17 20:14:03 -0300

    fixed sigabrt when erase pass

--- src/client/Login.cpp
+++ src/client/Login.cpp
@@ -207,7 +207,7 @@ int Login::runLoginWindow(char* ip, char* port) {
 
             if(canWrite){
                 if(e.type == SDL_KEYDOWN){
-                    if(e.key.keysym.sym == SDLK_BACKSPACE && (inputTextUser.length() > 0 || inputTextPsw.length() > 0)){
+                    if(e.key.keysym.sym == SDLK_BACKSPACE && (inputTextUser.length() > 0 && inputTextPsw.length() > 0)){
                         if(renderPass) inputTextPsw.pop_back();
                         else inputTextUser.pop_back();
                         renderText = true;
