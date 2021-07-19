12cd722acf10051d7dd41525507743570c97d42c
violetaperezandrade <viperez@fi.uba.ar>
2021-07-19 19:02:54 -0300

    muted sfxs

--- src/client/View.cpp
+++ src/client/View.cpp
@@ -269,7 +269,7 @@ void View::renderEntity(std::vector<Entity>::iterator it, std::vector<char>& sta
         std::string resize = "ABCDGHI";
         if (hammerStr.find(id) != std::string::npos){
             if (hammer == 0){
-                soundManager.playSoundFromState('h');
+                //soundManager.playSoundFromState('h');
                 hammer++;
             }
             if (resize.find(id) != std::string::npos){
@@ -452,7 +452,7 @@ int View::run() {
         renderEntity(entityVector.begin()+myCharacterPos, states, hammer);
         playerID = 0;
         logger.debugMsg("Fin de iteracion sobre vector de entidades", __FILE__, __LINE__);
-        soundManager.iterateStates(states);
+        //soundManager.iterateStates(states);
         sdlMngr.presentRender(windowRenderer);
         sdlMngr.clearRender(windowRenderer);
     }
